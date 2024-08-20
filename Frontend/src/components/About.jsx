5e19d3577a036be8c3804f0d04f96c89f829f648
rajpootSK <sk95022143@gmail.com>
2024-08-20 08:24:23 +0530

    first commit

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              Healthy food refers to food that contains the right amount of
+              nutrients to keep our body fit. We need healthy food to keep
+              ourselves fit. Furthermore, healthy food is also very delicious as
+              opposed to popular thinking. Nowadays, kids need to eat healthy
+              food more than ever.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ Frontend/src/components/Menu.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { data } from "../restApi.json";
+const Menu = () => {
+  return (
+    <>
+      <section className="menu" id="menu">
+        <div className="container">
+          <div className="heading_section">
+            <h1 className="heading">POPULAR DISHES</h1>
+            <p>
+              "I'm grapeful for this delicious meal!" "I'm souper excited about
+              this food adventure." "Stay cheesy and keep eating delicious
+              food." "I'm in a committed relationship with food."
+            </p>
+          </div>
+          <div className="dishes_container">
+            {data[0].dishes.map((element) => (
+              <div className="card" key={element.id}>
+                <img src={element.image} alt={element.title} />
+                <h3>{element.title}</h3>
+                <button>{element.category}</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Menu;
